61c411941253130eeca88505fd34eb8ab74227e3
Aleksey <kymotocycle@gmail.com>
2019-01-20 15:25:11 +0200

    create home and event components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventComponent } from './event/event.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
