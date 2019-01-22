4b02359bd1100ff88e7b7049dd929f675237d61a
Aleksey <kymotocycle@gmail.com>
2019-01-22 21:37:07 +0200

    Add events array for test, @import modal styles

--- src/app/public/home-page/home-page.component.ts
+++ src/app/public/home-page/home-page.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  events = [
+    {
+      title: 'Crazy run',
+      type: 1,
+      days: 20,
+      date: "12.09.11"
+    },
+    {
+      title: 'Lazy run',
+      type: 1,
+      days: 20,
+      date: "12.09.11"
+    },
+    {
+      title: 'Чика Чика, Вероника',
+      type: 4,
+      days: 20,
+      date: "12.09.11"
+    },
+    {
+      title: 'Бетман Бенедиктович',
+      type: 3,
+      days: 20,
+      date: "12.09.11"
+    },
+    {
+      title: 'Ран Вася Ран',
+      type: 4,
+      days: 20,
+      date: "12.09.11"
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
