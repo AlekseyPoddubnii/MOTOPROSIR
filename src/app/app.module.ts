f497c824bc76bfee3d8eaf5741c4f4b851568475
Aleksey <kymotocycle@gmail.com>
2019-01-24 19:37:01 +0200

    homepage events http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountModule } from './account/account.module';
 import { PublicModule } from './public/public.module';
-<<<<<<< HEAD
-import { LoginComponent } from './auth/login/login.component';
-<<<<<<< HEAD
-import { HttpClientModule } from '@angular/common/http';
-=======
-import { AuthComponent } from './auth/auth.component';
-import { AuthRoutingModule } from './auth/auth-routing.module';
 
-import { fakeBackendProvider } from './auth/helpers/fake-backend';
-import { JwtInterceptor} from './auth/helpers/jwt.interceptor';
-import {  ErrorInterceptor  } from './auth/helpers/error.interceptor';
-import { HomeComponent } from './auth/home/home.component';
-import { RegisterComponent } from './auth/register/register.component';
->>>>>>> master
-=======
->>>>>>> master
 
 @NgModule({
   declarations: [
@@ -32,31 +17,9 @@ import { RegisterComponent } from './auth/register/register.component';
     AppRoutingModule,
     AccountModule,
     PublicModule,
-<<<<<<< HEAD
-    MatDialogModule,
-<<<<<<< HEAD
     HttpClientModule
   ],
   providers: [HttpClientModule],
-=======
-    ReactiveFormsModule,
-    HttpClientModule,
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    // { provide: MatDialogRef, useValue: {} },
-    // { provide: MAT_DIALOG_DATA, useValue: [] },
-
-    // fake backend
-    fakeBackendProvider
-  ],
->>>>>>> master
-=======
-    HttpClientModule,
-  ],
-  providers: [],
->>>>>>> master
   bootstrap: [AppComponent],
   entryComponents: []
 })
---
+++ src/app/public/home-page/home-page.model.ts
@@ -0,0 +1,8 @@
+export class HomePage {
+  constructor(
+    public title: string,
+    public days: number,
+    public date: string,
+    public type: number
+  ) { }
+}
