80ca4da9247ba6da3e16d08f746b397f52067c4f
Aleksey <kymotocycle@gmail.com>
2019-01-24 19:11:51 +0200

    Del auth

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatDialog } from '@angular/material/dialog';
-
-import { LoginComponent } from '../auth/login/login.component';
-
 
 @Component({
   selector: 'app-public',
@@ -15,15 +11,10 @@ export class PublicComponent implements OnInit {
 
   constructor(
     private router: Router,
-    public dialog: MatDialog,
     ) {}
 
   ngOnInit() {
     this.router.navigate(['/index']);
-  }
-
-  public openModal() {
-    this.dialog.open(LoginComponent);
   }
 
 }
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -5,7 +5,6 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { PublicComponent } from './public.component';
 import { PublicRoutingModule } from './public-routing.module';
 import { AboutPageComponent } from './about-page/about-page.component';
-import { AuthRoutingModule } from '../auth/auth-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,8 +14,7 @@ import { AuthRoutingModule } from '../auth/auth-routing.module';
   ],
   imports: [
     CommonModule,
-    PublicRoutingModule,
-    AuthRoutingModule
+    PublicRoutingModule
   ]
 })
 
