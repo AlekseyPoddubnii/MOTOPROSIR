84d06b8ff2f312798d96abf6d9bcd4f14b166162
zzeenj <zzeenj@gmail.com>
2019-01-25 13:13:53 +0200

    fixed bugs with login

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,10 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { first } from 'rxjs/operators';
-
-import { User } from '../shared/models/user.model';
-import { AuthenticationService } from '../shared/services/authentication.service';
-import { Router } from '@angular/router';
-import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-account',
@@ -14,25 +8,9 @@ import { UserService } from '../shared/services/user.service';
 
 export class AccountComponent implements OnInit {
 
-    constructor(
-        private router: Router,
-        private authenticationService: AuthenticationService,
-        private userService: UserService
-    ) {
-        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-    }
-  currentUser: User;
+  constructor() {}
 
-    users: User[] = [];
+  ngOnInit() {
 
-    logout() {
-        this.authenticationService.logout();
-        this.router.navigate(['/index']);
-    }
-
-    ngOnInit() {
-        this.userService.getAll().pipe(first()).subscribe(users => {
-            this.users = users;
-        });
-    }
+  }
 }
